2e1f24b89bdb707df9a0266c3404e5b29e9e7d39
snehithshetty <50914520+snehithshetty@users.noreply.github.com>
2023-03-20 11:33:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -10,7 +11,6 @@ streamlit.text('🥑🍞Avacado toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Let's put a pick list here so they can pick the fruits they want to include
